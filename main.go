5efc6183ca0a9b5e6588090bac1c4bd82eb29fda
mars1385 <faceless1385@gmail.com>
2024-12-18 21:24:28 +0330

    currect trie data structure

--- main.go
+++ main.go
@@ -10,13 +10,13 @@ func main() {
 
 	obj := trie.Constructor()
 
-	obj.Insert("apple")
+	obj.Insert("hotdog")
 	// return True
 	// obj.Search("app")     // return False
 	// obj.StartsWith("app") // return True
 	// obj.Insert("app")
 	// obj.Search("app") // return True
 
-	fmt.Println(obj.Search("apple"))
+	fmt.Println(obj.Search("hot"))
 
 }
--- trie/implement-trie-prefix-tree.go
+++ trie/implement-trie-prefix-tree.go
@@ -1,30 +1,94 @@
 package trie
 
-import (
-	"strings"
-)
+// type Trie struct {
+// 	items map[string]bool
+// }
+
+// func Constructor() Trie {
+// 	return Trie{
+// 		items: map[string]bool{},
+// 	}
+// }
+
+// func (this *Trie) Insert(word string) {
+// 	this.items[word] = true
+// }
+
+// func (this *Trie) Search(word string) bool {
+// 	if len(this.items) <= 0 {
+// 		return false
+// 	}
+// 	_, has := this.items[word]
+
+// 	return has
+// }
+
+// func (this *Trie) StartsWith(prefix string) bool {
+
+// 	if len(this.items) <= 0 {
+// 		return false
+// 	}
+
+// 	for key := range this.items {
+
+// 		if strings.HasPrefix(key, prefix) {
+// 			return true
+// 		}
+// 	}
+
+// 	return false
+
+// }
 
 type Trie struct {
-	items map[string]bool
+	items     map[rune]*Trie
+	isEndLast bool
 }
 
 func Constructor() Trie {
 	return Trie{
-		items: map[string]bool{},
+		isEndLast: false,
+		items:     make(map[rune]*Trie),
 	}
 }
 
 func (this *Trie) Insert(word string) {
-	this.items[word] = true
+	current := this
+	for _, w := range word {
+
+		_, has := current.items[w]
+
+		if !has {
+
+			current.items[w] = &Trie{
+				isEndLast: false,
+				items:     make(map[rune]*Trie),
+			}
+
+		}
+
+		current = current.items[w]
+	}
+
+	current.isEndLast = true
 }
 
 func (this *Trie) Search(word string) bool {
 	if len(this.items) <= 0 {
 		return false
 	}
-	_, has := this.items[word]
 
-	return has
+	current := this
+	for _, w := range word {
+		_, has := current.items[w]
+		if !has {
+			return false
+		}
+		current = current.items[w]
+
+	}
+
+	return current.isEndLast
 }
 
 func (this *Trie) StartsWith(prefix string) bool {
@@ -32,14 +96,17 @@ func (this *Trie) StartsWith(prefix string) bool {
 	if len(this.items) <= 0 {
 		return false
 	}
+	current := this
+	for _, w := range prefix {
+		_, has := current.items[w]
 
-	for key := range this.items {
-
-		if strings.HasPrefix(key, prefix) {
-			return true
+		if !has {
+			return false
 		}
+		current = current.items[w]
+
 	}
 
-	return false
+	return current != nil
 
 }
